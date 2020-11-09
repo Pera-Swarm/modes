11efbc9c1632dbe25f32486027980867167238c5
luk3Sky <ganindudananja@gmail.com>
2020-11-10 01:09:25 +0530

    Added base mode functions

---
+++ mode.js
@@ -0,0 +1,31 @@
+/**
+ * @class Mode Representation
+ * @classdesc representing the mode controller for the swarm robot units and server
+ */
+class Mode {
+    /**
+     * Mode constructor
+     * @param {function} setup a function that runs at start
+     * @param {function} flow a function that runs periodically
+     * @param {number} interval the time interval of the flow function runs on
+     * @param {string} name name of the mode
+     */
+    constructor(setup, flow, interval, name) {
+        this.setup = setup;
+        this.flow = flow;
+        this.interval = interval;
+        this.name = name;
+    }
+
+    /**
+     * method for starting the mode
+     */
+    start = () => {
+        this.setup();
+        setInterval(() => {
+            this.flow();
+        }, this.interval);
+    };
+}
+
+module.exports = Mode;
